3bfede9bdf237e1698e358773dac6973e76e294b
HemangNakarani <hemangnh18@gmail.com>
2021-07-12 16:47:14 +0530

    fix: State of SpeechText in idle state, feat: suggestions in omnibox

--- src/background.ts
+++ src/background.ts
@@ -11,19 +11,30 @@ chrome.browserAction.onClicked.addListener(function () {
   });
 });
 
-chrome.omnibox.onInputEntered.addListener(function (text) {
+chrome.omnibox.onInputEntered.addListener(function (text, suggest) {
   switch (text) {
     case "open": {
       chrome.tabs.create({ url: "https://champagne.sprinklr.com" });
       break;
     }
-
     case "help": {
       chrome.tabs.create({
         url: `chrome-extension://${chrome.runtime.id}/options.html`,
       });
+      break;
+    }
+    default: {
+      console.log("Nothig Matched");
     }
   }
+  return;
+});
+
+chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
+  suggest([
+    { content: "open", description: "Open Sprinklr Website" },
+    { content: "help", description: "Open Options Page" },
+  ]);
 });
 
 // Open Options Page as Help Page on First Time and Also can be Visited via Options
--- src/components/VoiceCommandInterface/index.tsx
+++ src/components/VoiceCommandInterface/index.tsx
@@ -62,7 +62,6 @@ export default function VoiceCommandInterface(): ReactElement {
     Recognition.onresult = function (event: any) {
       var last = event.results.length - 1;
       var command: String = event.results[last][0].transcript;
-      console.log(command);
       setSpeechText(<>{command}</>);
       handleCommand(command);
     };
@@ -74,8 +73,14 @@ export default function VoiceCommandInterface(): ReactElement {
 
     Recognition.onerror = function (event: SpeechRecognitionErrorEvent) {
       handleStateChange(SpeechStateEnum.idle);
-      if(event.error ==="not-allowed"){
+      if (event.error === "not-allowed") {
         setMicrophonePermitted(false);
+      }
+      // condition for no-speech error
+      else if (event.error === "no-speech") {
+        setSpeechText(<>{"No Speech Recognised !!"}</>);
+      } else {
+        setSpeechText(<>{event.error}</>);
       }
     };
   }, []);
@@ -120,13 +125,16 @@ export default function VoiceCommandInterface(): ReactElement {
                 <span>{speechState}</span>
               </h1>
               <div className={style["bot-recognised-text"]}>
-                <span>{speechText}</span>
+                <span>
+                  {speechState === SpeechStateEnum.idle ? "..." : speechText}
+                </span>
               </div>
 
               <div></div>
 
               <div className={style["bot-mic-container"]}>
-                <div id="bot-volume-wave"
+                <div
+                  id="bot-volume-wave"
                   className={
                     speechState === SpeechStateEnum.listening
                       ? style["bot-mic-wave"]
