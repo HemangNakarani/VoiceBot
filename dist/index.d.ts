b8c6b8ae3eb8a41c59dc117914aaac0d715fc5b0
HemangNakarani <hemangnh18@gmail.com>
2021-07-18 00:17:11 +0530

    dist folder to publish

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+export declare function didComponentMount(selector: string): Promise<HTMLElement>;
+export declare function didChildComponentMount(selectorElement: HTMLElement, selector: string): Promise<HTMLElement>;
+export declare function didComponentsMount(selector: string): Promise<HTMLElement[]>;
+export declare function setInput(input: HTMLInputElement, value: string): void;
+export declare function addDelay(seconds: number): Promise<unknown>;
+export declare function scrollList(scrollableContainerSelectorString: string, listelementSelectorString: string, waitForFirstElementSelectorString: string, delayInMillisecondsAfterScroll: number, callback: (listElement: HTMLElement) => boolean): Promise<void>;
+export declare function click(selector: string): void;
