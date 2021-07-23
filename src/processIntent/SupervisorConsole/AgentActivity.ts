34694e42088b9a3e582a126459224a16030af516
HemangNakarani <hemangnh18@gmail.com>
2021-07-23 15:05:06 +0530

    feat: open agent activity and Smooth scroll

--- src/processIntent/SupervisorConsole/AgentActivity.ts
+++ src/processIntent/SupervisorConsole/AgentActivity.ts
@@ -1,20 +1,52 @@
-import { didComponentMount } from "voicebot-dommer";
+import {
+  addDelay,
+  didComponentMount,
+  didComponentsMount,
+} from "voicebot-dommer";
 
 export default async function OpenAgentActivity(agentname: string) {
-  // Need to write the code for selecting required agent-name
-  console.log(agentname);
+  let agentList = Array.from(
+    document.querySelectorAll(".fixedDataTableRowLayout_rowWrapper")
+  ) as HTMLElement[];
+  agentList.unshift();
 
+  let agentNameRegex = new RegExp(agentname, "ig");
   let eventHover = new Event("mouseover", {
     bubbles: true,
   });
 
-  let actions = document.querySelector(
-    `[aria-label="Row Actions"]`
-  ) as HTMLElement;
-  actions.dispatchEvent(eventHover);
+  agentList.forEach(async (node) => {
+    let textContent = node.querySelector(".agentProfile__name")?.textContent;
 
-  let activityButton = await didComponentMount(
-    `.popoverWithTwistyContainer [aria-label="View Activity"]`
-  );
-  activityButton.click();
+    if (textContent && textContent.match(agentNameRegex) && agentname !== "") {
+      let actions = node.querySelector(
+        `[aria-label="Row Actions"]`
+      ) as HTMLElement;
+      actions.dispatchEvent(eventHover);
+
+      let activityButton = await didComponentMount(
+        `.popoverWithTwistyContainer [aria-label="View Activity"]`
+      );
+      activityButton.click();
+      slowScroll(".user-activity__activitiesContainer", 1);
+    }
+  });
+}
+
+async function slowScroll(containerSelector: string, offset: number) {
+  let elem = await didComponentMount(containerSelector);
+
+  await addDelay(2000);
+
+  if (!elem) {
+    return;
+  }
+
+  let timer = setInterval(() => {
+    elem.scrollTop += offset;
+    if (elem.scrollTop === elem.scrollHeight - elem.clientHeight) {
+      console.log("auto scroll stopped");
+      clearInterval(timer);
+    }
+  }, 25);
 }
